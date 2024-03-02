6fd3709892a71cbd8fb4e9c40a3b33410542954a
Abdul-Rehman <144048502+AbdlReman@users.noreply.github.com>
2024-03-02 13:23:56 -0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -33,8 +33,14 @@ app.use(
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/blog", blogRouter);
 
+app.get("/",(req,res,next)=>{return res.status(200).json({
+  success:true,
+  message: "HELLO WORLD"
+})})
+
 dbConnection();
 
 app.use(errorMiddleware);
 
 export default app;
+
