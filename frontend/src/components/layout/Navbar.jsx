038f17b1afc205ff479a77f2431c68f85079bc6c
CoderMasterypro <abdulrehmanabbasi5067794@gmail.com>
2024-03-02 14:26:23 -0800

    Final

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
     e.preventDefault();
     try {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://cute-rose-firefly-wig.cyclic.app/api/v1/user/logout",
         { withCredentials: true }
       );
       setIsAuthenticated(false);
